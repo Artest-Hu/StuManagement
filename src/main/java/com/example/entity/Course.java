a80357f072157d24c3267d7fbce375dd11a0ff10
Artest_Hu <101932637+Artest-Hu@users.noreply.github.com>
2026-01-16 14:26:39 -0500

    Create Course.java

---
+++ src/main/java/com/example/entity/Course.java
@@ -0,0 +1,63 @@
+package com.example.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "course")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "course_name")
+    private String courseName;
+
+    @ManyToMany(mappedBy = "courses")
+    @JsonIgnoreProperties("courses")//prevent endless loop
+    @Column(name = "student_id")
+    private Set<Student> students = new HashSet<>();
+
+    public Course(Long id, String courseName) {
+        this.id = id;
+        this.courseName = courseName;
+    }
+
+    public Course() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
+}
