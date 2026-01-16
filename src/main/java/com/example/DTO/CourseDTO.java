9b2a03391174d4cb3e6bd0c8cc8d2479bb4d1fa6
Artest_Hu <101932637+Artest-Hu@users.noreply.github.com>
2026-01-16 14:23:20 -0500

    Create CourseDTO.java

---
+++ src/main/java/com/example/DTO/CourseDTO.java
@@ -0,0 +1,44 @@
+package com.example.DTO;
+
+import com.example.entity.Student;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CourseDTO {
+
+    private Long id;
+    private String courseName;
+
+    public CourseDTO() {
+    }
+
+    public CourseDTO(Long id, String courseName) {
+        this.id = id;
+        this.courseName = courseName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+}
